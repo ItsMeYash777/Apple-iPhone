fee9a1d49f2fe9ec4b549065c234547f22b1d2de
quarkspubg2@gmail.com <quarkspubg2@gmail.com>
2024-07-14 02:31:26 +0530

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import HowItWorks from "./components/HowItWorks";
+import * as Sentry from '@sentry/react'
 const App = () => {
+  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
   return (
    <main>
     <Navbar/>
     <Hero/>
     <Highlights/>
     <Model/>
+    <Features/>
+    <HowItWorks/>
+    <Footer/>
    </main>
   );
 }
 
-export default App
+export default Sentry.withProfiler(App)
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+//...
+import * as Sentry from "@sentry/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+Sentry.init({
+  dsn: "https://2b7c99ce1df4bc2c06a84eb9f8740c72@o4507596445581312.ingest.us.sentry.io/4507596448530432",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+
+    }),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
